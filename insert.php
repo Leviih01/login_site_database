a45b62b6970111787ffc5fe46736e8b3bc53cd2f
Levii01 <krozakxmail@gmail.com>
2022-09-01 18:39:44 +0200

    v2.8
    Database connection deployed

--- insert.php
+++ insert.php
@@ -1,4 +1,53 @@
 <?php
 $username = $_POST['username'];
-$password = $_POST['password']
+$password = $_POST['password'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+
+if (!empty($username) || !empty($password) || !empty($email)) {
+    # code...
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "login_sql";
+
+    //sql csatlakozás
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+    if (mysqli_connect_error()){
+        die('Connnect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else{
+        $SELECT = "SELECT email From register Where email = ? Limit 1";
+        $INSERT = "INSERT Into register ( username, password, email) values(?,?,?)";
+
+        $stmt = $conn->prepare($SELECT);
+        $stmt ->bind_param("s", $email
+        $stmt->execute();
+        $stmt->bind_result($resultEmail);
+        $stmt->store_result();
+        $stmt->fetch();
+        $rnum = $stmt->num_rows;
+        if ($rnum == 0) {
+            $stmt->close();
+            $stmt = $conn->prepare($Insert);
+            $stmt->bind_param("ssssii",$username, $password, $email);
+            if ($stmt->execute()) {
+                echo "Sikeres művelet";
+            }
+            else {
+                echo $stmt->error;
+            }
+        }
+        else {
+            echo "Valaki regisztrált már ezzel az email címmel.";
+        }
+        $stmt->close();
+        $conn->close();
+    }
+
+} else{
+    echo "Az összes kötelező!";
+    die();
+}
 ?>
+
