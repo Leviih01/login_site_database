4ae16b543550a510ca16ac57d53ca379237d66da
Levii01 <krozakxmail@gmail.com>
2022-09-09 19:14:17 +0200

    v2.9

--- insert.php
+++ insert.php
@@ -1,53 +1,55 @@
 <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-$gender = $_POST['gender'];
-$email = $_POST['email'];
-
-if (!empty($username) || !empty($password) || !empty($email)) {
-    # code...
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbname = "login_sql";
-
-    //sql csatlakozás
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-
-    if (mysqli_connect_error()){
-        die('Connnect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-    } else{
-        $SELECT = "SELECT email From register Where email = ? Limit 1";
-        $INSERT = "INSERT Into register ( username, password, email) values(?,?,?)";
-
-        $stmt = $conn->prepare($SELECT);
-        $stmt ->bind_param("s", $email
-        $stmt->execute();
-        $stmt->bind_result($resultEmail);
-        $stmt->store_result();
-        $stmt->fetch();
-        $rnum = $stmt->num_rows;
-        if ($rnum == 0) {
-            $stmt->close();
-            $stmt = $conn->prepare($Insert);
-            $stmt->bind_param("ssssii",$username, $password, $email);
-            if ($stmt->execute()) {
-                echo "Sikeres művelet";
-            }
-            else {
-                echo $stmt->error;
-            }
+if (isset($_POST['submit'])) {
+    if (isset($_POST['username']) && isset($_POST['password']) &&
+        isset($_POST['gender']) && isset($_POST['email']) &&
+        isset($_POST['phoneCode']) && isset($_POST['phone'])) {
+        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "login_sql";
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
         }
         else {
-            echo "Valaki regisztrált már ezzel az email címmel.";
+            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO register(username, password, email) values(?, ?, ?)";
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+            if ($rnum == 0) {
+                $stmt->close();
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssssii",$username, $password, $gender, $email, $phoneCode, $phone);
+                if ($stmt->execute()) {
+                    echo "Sikeres művelet";
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Valaki regisztrált már ezzel az email címmel.";
+            }
+            $stmt->close();
+            $conn->close();
         }
-        $stmt->close();
-        $conn->close();
     }
-
-} else{
-    echo "Az összes kötelező!";
-    die();
+    else {
+        echo "Az összes kötelező!";
+        die();
+    }
+}
+else {
+    echo "A gomb nincs konfigurálva";
 }
 ?>
-
